d31a88516868388cb49cee10a36a4801b12050eb
nitin <nitingojiya2000@gmail.com>
2025-06-11 10:54:41 +0530

    fd test cases

---
+++ fd/fd.rb
@@ -0,0 +1,4 @@
+
+# for year  (55555 * ((1 + 5.5/400.0)**(4*5))).round
+# for months
+# "Hello".casecmp("hell") 1 min false and 0 true
---
+++ fd/fd_test.rb
@@ -0,0 +1,108 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+require_relative 'fd'
+
+# fd calculator test cases
+class FdTest < Minitest::Test
+  def test_total_investment_low
+    assert_raises(ArgumentError, 'Total Investment is Between 5000 to 10000000') { Fd.new(500, 12) }
+  end
+
+  def test_total_investment_high
+    assert_raises(ArgumentError, 'Total Investment is Between 5000 to 10000000') { Fd.new(500_000_00, 12) }
+  end
+
+  def test_total_investment_not_allow_decimal
+    assert_raises(ArgumentError, 'Total Investment is only integer') { Fd.new(224_00.04, 12) }
+  end
+
+  def test_rate_low
+    assert_raises(ArgumentError, 'Rate 1 to 15') { Fd.new(5000, 0) }
+  end
+
+  def test_rate_high
+    assert_raises(ArgumentError, 'Rate 1 to 15') { Fd.new(5000, 24) }
+  end
+
+  def test_period_type_days_limit_low
+    assert_raises(ArgumentError, 'Days 1 to 31') { Fd.new(5000, 4, 0, 1) }
+  end
+
+  def test_period_type_days_limit_high
+    assert_raises(ArgumentError, 'Days 1 to 31') { Fd.new(5000, 2, 32, 1) }
+  end
+
+  def test_period_type_months_limit_low
+    assert_raises(ArgumentError, 'Months 1 to 11') { Fd.new(5000, 5, -1, 2) }
+  end
+
+  def test_period_type_months_limit_high
+    assert_raises(ArgumentError, 'Months 1 to 11') { Fd.new(5000, 5, 12, 2) }
+  end
+
+  def test_period_type_year_limit_low
+    assert_raises(ArgumentError, 'Years 1 to 25') { Fd.new(5000, 7, -12, 3) }
+  end
+
+  def test_period_type_year_limit_high
+    assert_raises(ArgumentError, 'Years 1 to 25') { Fd.new(5000, 7, 28, 3) }
+  end
+
+  # yearly calculate
+  def test_total_cal_yearly_fd_one
+    fd = Fd.new(555_55, 10.5, 5, 3)
+    expected = { total_value: 932_80, est_return: 377_25, investment_amount: 555_55 }
+    assert_equal expected, fd.cal_fd
+  end
+
+  def test_total_cal_yearly_fd_two
+    fd = Fd.new(216_500_0, 7.5, 18, 3)
+    expected = { total_value: 824_758_2, est_return: 608_258_2, investment_amount: 216_500_0 }
+    assert_equal expected, fd.cal_fd
+  end
+
+  def test_total_cal_yearly_fd_three
+    fd = Fd.new(100_000_00, 15, 25, 3)
+    expected = { total_value: 397_018_312, est_return: 387_018_312, investment_amount: 100_000_00 }
+    assert_equal expected, fd.cal_fd
+  end
+
+  # monthly calculate
+  def test_total_cal_monthly_fd_one
+    fd = Fd.new(555_55, 10.5, 5, 2)
+    expected = { total_value: 580_05, est_return: 2450, investment_amount: 555_55 }
+    assert_equal expected, fd.cal_fd
+  end
+
+  def test_total_cal_monthly_fd_two
+    fd = Fd.new(216_500_0, 7.5, 11, 2)
+    expected = { total_value: 231_817_8, est_return: 153_178, investment_amount: 216_500_0 }
+    assert_equal expected, fd.cal_fd
+  end
+
+  def test_total_cal_monthly_fd_three
+    fd = Fd.new(100_000_00, 15, 5, 2)
+    expected = { total_value: 106_300_00, est_return: 630_000, investment_amount: 100_000_00 }
+    assert_equal expected, fd.cal_fd
+  end
+
+  # Days calculate
+  def test_total_cal_days_fd_one
+    fd = Fd.new(555_55, 10.5, 30, 1)
+    expected = { total_value: 560_33, est_return: 478, investment_amount: 555_55 }
+    assert_equal expected, fd.cal_fd
+  end
+
+  def test_total_cal_days_fd_two
+    fd = Fd.new(216_500_0, 7.5, 11, 1)
+    expected = { total_value: 216_987_1, est_return: 4871, investment_amount: 216_500_0 }
+    assert_equal expected, fd.cal_fd
+  end
+
+  def test_total_cal_days_fd_three
+    fd = Fd.new(100_000_00, 15, 5, 1)
+    expected = { total_value: 100_210_00, est_return: 210_00, investment_amount: 100_000_00 }
+    assert_equal expected, fd.cal_fd
+  end
+end
